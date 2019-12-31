a545da824e0d59989c0f9778e6a1ed1ca1769fe6
akash2305-Git <58457390+akash2305-Git@users.noreply.github.com>
2019-12-31 13:07:28 +0530

    Add files via upload

---
+++ Operator1.py
@@ -0,0 +1,50 @@
+
+
+
+
+x = 100
+y = 200
+print(x and y)
+
+print(x or y)
+
+print(not x)
+
+
+a=1; b=2 ;c=3
+if(a<b and b<c):
+    print('yes')
+else :
+        print('no')
+
+x=int(input('Enter a digit: '))
+if x==0:
+    print("zero")
+elif x==1 :
+    print("one")
+elif x==2 :
+    print("two")
+elif x==3 :
+    print("three")
+elif x==4 :
+    print("four")
+elif x==5 :
+    print("five")
+elif x==6 :
+    print("six")
+elif x==7 :
+    print("seven")
+elif x==8 :
+    print("eight")
+elif x==9 :
+    print("none")
+else :
+    print("Enter number from 0 to 9")
+
+        
+
+
+
+
+ 
+ 
---
+++ str.py
@@ -0,0 +1,6 @@
+s ='welcome to core python'
+print(s)
+print(s[0])
+print(s[3:7])
+print(s[11:])
+print(s[-1])
